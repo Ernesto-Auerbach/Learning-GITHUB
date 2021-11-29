8fa42db503d54b6fe06da166c12b8cb8cb68ca03
Ernesto Auerbach <auerbachrea@gmail.com>
2021-11-29 08:46:22 -0600

    Initial Commit

---
+++ Learning GitHub/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Learning GitHub
+//
+//  Created by AM Rust - Ernesto Auerbach  on 11/29/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
